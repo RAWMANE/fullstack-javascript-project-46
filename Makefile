be01014669868d27979b47a249a51d8574eb0ef0
root <root@DESKTOP-LRGHUTA.localdomain>
2023-05-11 10:36:04 -0700

    commit step-2

--- Makefile
+++ Makefile
@@ -1,7 +1,13 @@
 install :
 	npm install
 
+lint:
+	npx eslint .
+
+fix:
+	npx eslint . --fix
+
 publish :
 	npm publish --dry-run
 
- 
+
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
-  
+
 program
   .version('0.0.1')
+  .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f,--formant <type>', 'output format')
   .name('gendiff')
-  .parse()
-  
+  .parse();
+ 
